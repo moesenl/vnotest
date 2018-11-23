d070d90e738b3b073f66eff95ed3e143083f5c9c
luc vermoesen <lucvermoesen@gmail.com>
2018-11-23 15:03:30 +0000

    tested version that merely demonstrates this plugin fits in the framework No real acceptance test yet

--- vnotest_tempest_plugin/tests/api/test_vnoacceptance.py
+++ vnotest_tempest_plugin/tests/api/test_vnoacceptance.py
@@ -1,31 +1,15 @@
 from vnotest_tempest_plugin.tests.api import base
 from tempest.lib import decorators
 
-# import pudb
-
 class TestVNOAcceptance(base.BaseVNOAcceptanceTest):
     """Here we test the basic operations of images"""
 
     @classmethod
     def skip_checks(cls):
-#        pudb.set_trace()
         super(TestVNOAcceptance, cls).skip_checks()
-
-    @classmethod
-    def resource_setup(cls):
-        super(TestVNOAcceptance, cls).resource_setup()
 
     @decorators.attr(type="smoke")
     @decorators.idempotent_id('f949ba93-1c6e-45a4-8726-39e3a6a38624')
     def test_vnoacceptance(self):
-#        pudb.set_trace()
         self.assertEqual('Hello VNO!', 'Hello VNO!')
-
-    @classmethod
-    def resource_cleanup(cls):
-        super(TestVNOAcceptance, cls).resource_cleanup()
-
-#    @classmethod
-#    def setUpClass(cls):
-#        super(TestVNOAcceptance, cls).setUpClass()
 
